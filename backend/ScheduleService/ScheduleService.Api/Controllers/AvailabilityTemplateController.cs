fe59422e4eb7b46e0f31441764e0dc89ff89661a
Dragoh54 <ndragoh@gmail.com>
2025-04-17 11:13:46 +0300

    feat: restored controller

--- backend/ScheduleService/ScheduleService.Api/Controllers/AvailabilityTemplateController.cs
+++ backend/ScheduleService/ScheduleService.Api/Controllers/AvailabilityTemplateController.cs
@@ -1,6 +1,11 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ScheduleService.Application.UseCases.AvailabilityTemplate.Command.AddTemplateCommand;
+using ScheduleService.Application.UseCases.AvailabilityTemplate.Command.DeleteTemplateCommand;
+using ScheduleService.Application.UseCases.AvailabilityTemplate.Command.SetToDefaultCommand;
+using ScheduleService.Application.UseCases.AvailabilityTemplate.Command.UpdateTemplateCommand;
+using ScheduleService.Application.UseCases.AvailabilityTemplate.Query.GetByIdTemplateQuery;
+using ScheduleService.Application.UseCases.AvailabilityTemplate.Query.GetDefaultTemplateQuery;
 using ScheduleService.Application.UseCases.AvailabilityTemplate.Query.GetUserTemplatesQuery;
 
 namespace ScheduleService.Api.Controllers;
@@ -13,16 +18,52 @@ public class AvailabilityTemplateController(
 {
 
     [HttpPost]
-    public async Task<IResult> AddAvailabilityTemplate([FromBody] AddTemplateCommand template, CancellationToken cancellationToken)
+    public async Task<IResult> AddAvailabilityTemplate([FromBody] AddTemplateCommand command, CancellationToken cancellationToken)
     {
-        var newTemplate = await mediator.Send(template, cancellationToken);
+        var newTemplate = await mediator.Send(command, cancellationToken);
         return Results.Ok(newTemplate);
     }
 
-    [HttpGet]
+    [HttpPut]
+    public async Task<IResult> UpdateAvailabilityTemplate([FromBody] UpdateTemplateCommand command, CancellationToken cancellationToken)
+    {
+        var updatedTemplate = await mediator.Send(command, cancellationToken);
+        return Results.Ok(updatedTemplate);
+    }
+
+    [HttpDelete]
+    public async Task<IResult> DeleteAvailabilityTemplate([FromQuery] DeleteTemplateCommand command, CancellationToken cancellationToken)
+    {
+        var updatedTemplate = await mediator.Send(command, cancellationToken);
+        return Results.Ok(updatedTemplate);
+    }
+
+    [HttpPatch]
+    public async Task<IResult> SetToDefaultTemplate([FromQuery] SetToDefaultCommand command, CancellationToken cancellationToken)
+    {
+        var updatedTemplate = await mediator.Send(command, cancellationToken);
+        return Results.Ok(updatedTemplate);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IResult> GetAvailabilityTemplate([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetByIdTemplateQuery { Id = id };
+        var updatedTemplate = await mediator.Send(query, cancellationToken);
+        return Results.Ok(updatedTemplate);
+    }
+
+    [HttpGet("me")]
     public async Task<IResult> GetUserAvailabilityTemplates([FromQuery] GetUserTemplatesQuery query, CancellationToken cancellationToken)
     {
-        var newTemplate = await mediator.Send(query, cancellationToken);
-        return Results.Ok(newTemplate);
+        var templates = await mediator.Send(query, cancellationToken);
+        return Results.Ok(templates);
+    }
+
+    [HttpGet("default")]
+    public async Task<IResult> GetDefaultTemplate([FromQuery] GetDefaultTemplateQuery query, CancellationToken cancellationToken)
+    {
+        var template = await mediator.Send(query, cancellationToken);
+        return Results.Ok(template);
     }
 }
